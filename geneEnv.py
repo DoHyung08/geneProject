956a56fe76c18560b1f8a7ed559db6640ae1a3c6
DoHyung08 <bigpig3673@gmail.com>
2024-09-08 15:59:51 +0900

    all error clear. hehehe

--- geneEnv.py
+++ geneEnv.py
@@ -11,8 +11,8 @@ class geneEnv(Env):
         self.generation_popul = generation_popul
         self.geneNum = geneNum
 
-        action_ranges = [generation_popul] * generation_popul * geneNum
-        observ_ranges = [len(pig.alleleInt)] * generation_popul * geneNum
+        action_ranges = [generation_popul] * (generation_popul * geneNum)
+        observ_ranges = [len(pig.phenoInt)] * (generation_popul * geneNum)
         self.action_space = spaces.MultiDiscrete(action_ranges)
         self.observation_space = spaces.MultiDiscrete(observ_ranges)
         self.state = self.make_generation()
@@ -33,7 +33,7 @@ class geneEnv(Env):
         random.seed(seed)
 
 
-    def get_obs(self):
+    def get_obs(self):#표현형을 숫자 리스트로 변환
         obs_list = []
 
         for pigG in self.state:
@@ -51,15 +51,16 @@ class geneEnv(Env):
         return obs_list
 
 
-    def step(self, action):#action은 세대 순서를 재배치한 벡터, action[i]=i허용
+    def step(self, action):#action은 세대 순서를 재배치한 벡터, action[i]=i허용 #안겹치게 추가해야함
+
         nextGen = []
         self.episode_len -= 1
 
         for i in range(0,len(action)-1,2):
             for j in range(2):
                 next_gene = []
-                gene1 = self.state[i].trans()
-                gene2 = self.state[i+1].trans()   
+                gene1 = self.state[action[i]].trans()
+                gene2 = self.state[action[i+1]].trans()   
 
                 for g in range(self.geneNum):
                     next_gene.append([gene1[g],gene2[g]])##2 allele
@@ -68,7 +69,7 @@ class geneEnv(Env):
         
         reward = 0
         totalPheno = []
-        for g in range(self.numGen):
+        for g in range(self.geneNum):
             phenoCnt = [0 for i in range(len(pig.phenoInt))]
 
             for babyPig in nextGen:
@@ -81,10 +82,10 @@ class geneEnv(Env):
             done = True
         else:
             done = False
-
+        truncated = False
         info = {}
 
-        return self.get_obs(), reward, done, info
+        return self.get_obs(), reward, done, truncated, info
         
 
     def make_generation(self):
--- train.py
+++ train.py
@@ -1,11 +1,18 @@
 import gymnasium as gym
 
 from stable_baselines3 import PPO
-from stable_baselines3 import A2C
-from stable_baselines3.common.env_util import make_atari_env
+#from stable_baselines3 import A2C
+#from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 from geneEnv import geneEnv
 
+
+
 env = geneEnv(1,20,100)
-model = PPO("MlpPolicy", env, verbose=1)
+
+# print(f"Observation space: {env.observation_space}")
+# print(f"Action space: {env.action_space}")
+# print(f"Sample observation: {env.reset()}")
+
+model = PPO("MlpPolicy", env, verbose=1,device='cpu')
 model.learn(total_timesteps=10_000)
