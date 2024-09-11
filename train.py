c6bc4518f125d68919d8765c43973afe54f23f80
DoHyung08 <bigpig3673@gmail.com>
2024-09-11 21:54:26 +0900

    trained

--- train.py
+++ train.py
@@ -14,6 +14,6 @@ env = geneEnv(1,50,100)
 # print(f"Action space: {env.action_space}")
 # print(f"Sample observation: {env.reset()}")
 
-model = PPO("MlpPolicy", env, verbose=1,device='cpu')
+model = PPO("MlpPolicy", env, verbose=1,device='cpu',learning_rate=0.003, gamma=0.2,n_epochs=20)
 model.learn(total_timesteps=100_0000)
 model.save("trained_model")
